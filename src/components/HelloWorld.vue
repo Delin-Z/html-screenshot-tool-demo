d0fcc1ad38465ea82972342128ec1e0b5571bf76
Delin <delin@iqtogether.com>
2020-06-19 11:32:03 +0800

    优化

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,11 +1,12 @@
 <template>
   <div>
-    <h1>{{ msg }}</h1>
-    <p>
-      <a href="https://cli.vuejs.org" target="_blank">vue-cli documentation</a>
-    </p>
+    <h1>Welcome to Your Vue.js App</h1>
     <button @click="fullscreen()">全屏截图</button>
     <button @click="startScreenshot()">开始截图</button>
+    <div>预览区域</div>
+    <div class="view_box">
+      <img :src="imgUrl"/>
+    </div>
   </div>
 </template>
 
@@ -13,42 +14,47 @@
 import screenshot from 'html-screenshot-tool'
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  data () {
+    return {
+      imgUrl: null
+    }
   },
   methods: {
     fullscreen: function(){
-      let options = {
+      screenshot.fullscreen({
         successCallback: this.finishClip // 上传成功回调 
-      }
-      screenshot.fullscreen(options)
+      })
     },
     startScreenshot: function(){
-      let options = {
+      screenshot.start({
         successCallback: this.finishClip, // 上传成功回调 
         cancelCallback: this.cancelClip // 取消截图回调 
-      }
-      screenshot.start(options)
+      })
     },
-    finishClip: function(){
+    finishClip: function(val){
+      this.imgUrl = val
     },
     cancelClip: function(){
+      console.log('您取消了截图')
     }
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-button{
+button {
   background: #10b93b;
   border: none;
   color: white;
   border-radius: 5px;
   padding: 5px 9px;
   margin: 10px;
+}
+.view_box {
+  border: 1px solid #ccc;
+  min-height: 200px;
+}
+.view_box img {
+  max-width: 90%;
 }
 </style>
